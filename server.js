81edc04ac77e561d825f84eaabb74c134f2026ef
aeife <andre.eife@gmail.com>
2015-05-01 20:28:13 +0200

    adjusted connection settings for local dev

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ app.use(expressSession({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
 
