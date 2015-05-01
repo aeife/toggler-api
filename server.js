92f7d1040e3c77097c7409d0cb0534c75421657f
aeife <andre.eife@gmail.com>
2015-05-01 16:17:26 +0200

    allow cross origin requests

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(expressSession({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 var router = express.Router();
 router.get('/', function(req, res) {
